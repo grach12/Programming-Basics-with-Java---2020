4d39ae11451f9b9561abfcaaad40cd33d13f4b2a
radoslav_radev@hotmail.com <Ardiop12>
2020-04-07 14:07:47 +0300

    For - цикъл – допълнително упражнение: задача Завръщане в бъдещето.

--- Loops/src/BackToThePast.java
+++ Loops/src/BackToThePast.java
@@ -7,23 +7,48 @@ public class BackToThePast {
         double money = Double.parseDouble(scan.nextLine());
         int year = Integer.parseInt(scan.nextLine());
 
-        double moneyToLive = 12000;
+        double moneyToleave = 0;
 
-        for (int i = 0; i <year ; i++) {
-            if (year%2==0){
-                money=money-12000;
+        for (int i = 0; i <= year - 1800; i++) {
+            //  System.out.println(i);
+            //moneyToleave = money; //така не се прави това се нулира всеки път.
+            if (i % 2 == 0) {
+                moneyToleave = moneyToleave - 12000;
             }
-            else {
-                money=money-1200+50*(18+year);
+            if (!(i % 2 == 0)) {
+                moneyToleave = moneyToleave - (12000 + 50 * (18 + i));
             }
-             money=money-moneyToLive ;
-
-             if (money<=0){
-                 System.out.printf("He will need 12399.85 %.2f dollars to survive.",money);
-                 break;
-             }
-
         }
+        double moneyRest = money + moneyToleave;
+        if (moneyRest >= 0) {
+            System.out.printf("Yes! He will live a carefree life and will have %.2f dollars left.", moneyRest);
+        } else {
+            System.out.printf("He will need %.2f dollars to survive.", Math.abs(moneyRest));
+        }
+        //  System.out.println(moneyToleave);
+        //  System.out.println(money);
+        //  System.out.println(money + moneyToleave);
     }
 }
 
+//        double money = Double.parseDouble(scan.nextLine());
+//        int year = Integer.parseInt(scan.nextLine());
+//
+//        double moneyTolive=0;
+//
+//        for (int i = 0; i <=year-1800 ; i++) {
+//            double evenYearExpence = 12000;
+//            double oddYearExpence = (12000+50*(18+i));;
+//
+//
+//            if (i%2==0){
+//                moneyTolive=moneyTolive-evenYearExpence;
+//            }
+//            if (!(i%2== 0)) {
+//                moneyTolive=moneyTolive-oddYearExpence;
+//            }
+//        }System.out.println(money-moneyTolive);
+//        //System.out.println(money);
+//    }
+//}
+//
