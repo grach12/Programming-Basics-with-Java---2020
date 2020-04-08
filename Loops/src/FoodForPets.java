daeb1a7a4190768fa58ed23df49b7ae552a6ec82
Sector <radoslav_radev@hotmail.com>
2020-04-09 00:02:19 +0300

    Изпитни задачи : for loops : Храна за домашни любимци

--- Loops/src/FoodForPets.java
+++ Loops/src/FoodForPets.java
@@ -8,16 +8,27 @@ public class FoodForPets {
         int days = Integer.parseInt(scan.nextLine());
         double totalFood = Double.parseDouble(scan.nextLine());
 
-
+double cookies = 0;
+double cats=0;
+double dogs=0;
 
         for (int i = 0; i <days ; i++) {
             double dogFood = Double.parseDouble(scan.nextLine());
             double catFood = Double.parseDouble(scan.nextLine());
+dogs = dogs+dogFood;
+cats= cats+catFood;
+totalFood=dogs+cats; // на прав път сме
+
+            if (i % 3 == 0) {
+                cookies=(dogFood+catFood)*0.9;
+                cookies++;
+            }
+
 
 
         }
 
-
+        System.out.println(totalFood);
 
     }
 }
