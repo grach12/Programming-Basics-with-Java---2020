c57adde5c8fa724924bd1fcf487d6a06a691fd94
radoslav_radev@hotmail.com <Ardiop12>
2020-04-08 15:34:26 +0300

    For - цикъл – Изпитни задачи Задача 4. Храна за домашни любимци

---
+++ Loops/src/FoodForPets.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class FoodForPets {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+        int days = Integer.parseInt(scan.nextLine());
+        double totalFood = Double.parseDouble(scan.nextLine());
+
+
+
+        for (int i = 0; i <days ; i++) {
+            double dogFood = Double.parseDouble(scan.nextLine());
+            double catFood = Double.parseDouble(scan.nextLine());
+
+
+        }
+
+
+
+    }
+}
