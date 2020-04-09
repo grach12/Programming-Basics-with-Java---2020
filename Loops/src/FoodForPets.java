7f8527ca4b6a6bbca9468d681788bf6c8594e99b
radoslav_radev@hotmail.com <Ardiop12>
2020-04-09 11:17:54 +0300

    For - цикъл – допълнително упражнение Начало

--- Loops/src/FoodForPets.java
+++ Loops/src/FoodForPets.java
@@ -11,24 +11,24 @@ public class FoodForPets {
 double cookies = 0;
 double cats=0;
 double dogs=0;
+double eatenFood = totalFood;
 
         for (int i = 0; i <days ; i++) {
             double dogFood = Double.parseDouble(scan.nextLine());
             double catFood = Double.parseDouble(scan.nextLine());
-dogs = dogs+dogFood;
-cats= cats+catFood;
-totalFood=dogs+cats; // на прав път сме
+            dogs = dogs+dogFood;
+            cats= cats+catFood;
+            eatenFood=dogs+cats;
 
-            if (i % 3 == 0) {
-                cookies=(dogFood+catFood)*0.9;
-                cookies++;
+            if (days % 3 == 0) {
+               // System.out.println(dogFood+catFood);
+                cookies=(dogFood+catFood)*0.1;
+                //cookies++;
             }
-
-
-
         }
-
-        System.out.println(totalFood);
-
+        System.out.printf("Total eaten biscuits: %.0fgr.%n",cookies);
+        System.out.printf("%.2f%% of the food has been eaten.%n",eatenFood/totalFood*100);
+        System.out.printf("%.2f%% eaten from the dog.%n",dogs/eatenFood*100);
+        System.out.printf("%.2f%% eaten from the cat.",cats/eatenFood*100);
     }
 }
