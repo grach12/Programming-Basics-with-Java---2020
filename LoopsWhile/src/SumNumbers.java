8ad958adaaba2fae690a64385d7f4228b8de71a0
Sector <radoslav_radev@hotmail.com>
2020-04-12 11:15:12 +0300

    While Loops Exercise. Sum Numbers

---
+++ LoopsWhile/src/SumNumbers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SumNumbers {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        int sum = 0;
+
+        while (!input.equals("Stop")){
+            int number = Integer.parseInt(input);
+
+            sum+=number;
+
+            input=scan.nextLine();
+        }
+        System.out.println(sum);
+    }
+}
