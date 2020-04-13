1af2e8bcd83d003edb2a1effddcf4e3621cb9688
radoslav_radev@hotmail.com <Ardiop12>
2020-04-13 10:14:40 +0300

    Задача Почивка решена , но в Judge дава 77/100

--- LoopsWhile/src/Vacation.java
+++ LoopsWhile/src/Vacation.java
@@ -6,33 +6,33 @@ public class Vacation {
 
         double target = Double.parseDouble(scan.nextLine());
         double startMoney = Double.parseDouble(scan.nextLine());
-int spendCounter =0;
-int daysCounter =0;
-while (startMoney<target){
-    String activity = scan.nextLine();
-    double money = Double.parseDouble(scan.nextLine());
-    if ("spend".equals(activity)){
+        int spendCounter = 0;
+        int daysCounter = 0;
+        while (startMoney < target) {
+            String activity = scan.nextLine();
+            double money = Double.parseDouble(scan.nextLine());
+            if ("spend".equals(activity)) {
 
-        if (startMoney-money>0){
-            startMoney=startMoney-money;
-        }else  {
-            startMoney=0;
+                if (startMoney - money > 0) {
+                    startMoney = startMoney - money;
+                } else {
+                    startMoney = 0;
+                }
+                spendCounter++;
+                if (spendCounter == 5) {
+                    break;
+                }
+            } else if ("save".equals(activity)) {
+                startMoney = startMoney + money;
+                spendCounter = 0;
+            }
+            daysCounter++;
         }
-        spendCounter++;
-        if (spendCounter==5){
-            break;
+        if (spendCounter == 5) {
+            System.out.println("You can't save the money.");
+            System.out.println(daysCounter);
+        } else {
+            System.out.printf("You saved the money for %d days.", daysCounter);
         }
-    }
-    else if ("save".equals(activity)){
-        startMoney=startMoney+money;
-        spendCounter=0;
-    }
-}
-if (spendCounter==5){
-    System.out.printf("You cant save money");
-    System.out.println(daysCounter);
-}else {
-    System.out.printf("You saved", daysCounter);
-}
     }
 }
