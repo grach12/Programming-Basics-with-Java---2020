c84c9aa9167b4ed4b45c86385557542034527cad
Sector <radoslav_radev@hotmail.com>
2020-04-13 23:23:10 +0300

    Изпит по "Основи на програмирането" – 6 и 7 юли 2019 Club , решението на Наталия

--- LoopsWhile/src/Club.java
+++ LoopsWhile/src/Club.java
@@ -7,26 +7,60 @@ public class Club {
         int profit = Integer.parseInt(scan.nextLine());
 
         String coctail = scan.nextLine();
-
         int price = coctail.length();
-        double priceDoub = 0.0;
         //System.out.println(price);
 
         while (!"Party".equals(coctail)) {
-
+            coctail= scan.nextLine(); // тук гърми 2-я текст
             int orders = Integer.parseInt(coctail);
             price = price + orders;
-            priceDoub = price;
 
-
-            if (priceDoub % 2 != 0) {
-                priceDoub *= 0.75;
+            if (price*1.0 % 2 != 0) {
+                price *= 0.75;
             }
-
-
-            coctail = scan.nextLine();
+           // coctail = scan.nextLine(); // тук гърми 1-я текст
         }
-
         System.out.println(price);
     }
 }
+
+/*
+import java.util.Scanner;
+
+public class Club {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        double wantedMoney = Double.parseDouble(sc.nextLine());
+
+        double gainedMoney = 0.0;
+
+        while (gainedMoney <= wantedMoney) {
+            String cocktailName = sc.nextLine();
+            if ("Party!".equals(cocktailName)) {
+                break;
+            }
+            int numberCocktails = Integer.parseInt(sc.nextLine());
+            int cocktailPrice = cocktailName.length();
+            double orderPrice = numberCocktails * cocktailPrice;
+            if (orderPrice % 2 == 1) {
+                orderPrice *= 0.75;
+            }
+            gainedMoney += orderPrice;
+
+        }
+
+        if (gainedMoney >= wantedMoney) {
+            System.out.println("Target acquired.");
+            System.out.printf("Club income - %.2f leva.", gainedMoney);
+        } else {
+            double diff = wantedMoney - gainedMoney;
+            System.out.printf("We need %.2f leva more.", diff);
+            System.out.printf("Club income - %.2f leva.", gainedMoney);
+
+        }
+    }
+}
+
+ */
