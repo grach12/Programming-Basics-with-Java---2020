10119cd2f055418511c850e8ab1916dbc29e6cfa
radoslav_radev@hotmail.com <Ardiop12>
2020-04-23 17:09:27 +0300

    Изпит по "Основи на програмирането" - 2 и 3 Май 2019 Задача 6. Ветеринарен Паркинг

---
+++ NestedLoops/src/VetParking.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class VetParking {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int days = Integer.parseInt(scan.nextLine());
+        int hours = Integer.parseInt(scan.nextLine());
+
+        double ticket = 0;
+        double sumDay = 0;
+double sumTicket = 0 ;
+
+        for (int i = 0; i < days; i++) {
+
+            for (int j = 0; j < hours; j++) {
+
+                if ((j % 2 != 0) && (i % 2 == 0)) {
+                    ticket = 1.25;
+                } else if ((j % 2 == 0) && (i % 2 != 0)) {
+                    ticket = 2.50;
+                } else {
+                    ticket = 1;
+                }
+                sumDay += ticket;
+
+            }
+            sumTicket = ticket;
+            System.out.println("SumDay " + sumTicket);
+        }
+
+        System.out.println("SumTotal " + sumDay);
+
+    }
+}
