b66bd4569306376e7fcde450a7a268456f47fe1b
radoslav_radev@hotmail.com <Ardiop12>
2020-04-24 15:51:04 +0300

    More Exercise While loops : Stream of Letters

---
+++ LoopsWhile/src/StreamOfLetters.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class StreamOfLetters {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String input  = scan.nextLine();
+   //     input = input.replaceAll("[^\\p{ASCII}]", "");
+
+        while (!"End".equals(input)){
+             char n  = input.charAt(0);
+             if ((n<32) || (n>127)){
+                 break;
+            }
+
+
+
+
+            System.out.println(n);
+
+
+
+            input= scan.nextLine();
+        }
+
+        System.out.println(input);
+    }
+}
