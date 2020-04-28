03a60890f69db0c84ee4fd8c13e4c91da208f55c
TV <radoslav_radev@hotmail.com>
2020-04-28 13:47:56 +0300

    Nested Loops Exercise 5. Password Generator (not included in final score)

--- NestedLoops/src/PasswordGenerator.java
+++ NestedLoops/src/PasswordGenerator.java
@@ -7,12 +7,12 @@ public class PasswordGenerator {
         int n = Integer.parseInt(scan.nextLine());
         int l = Integer.parseInt(scan.nextLine());
 
-        for (int i = 0; i < n; i++) { //symbol 1
-            for (int j = 0; j < n; j++) { //symbol2
+        for (int i = 1; i < n; i++) { //symbol 1
+            for (int j = 1; j < n; j++) { //symbol2
                 for (char k = 'a'; k < 'a' + l; k++) {
                     for (char m = 'a'; m < 'a' + l; m++) {
-                        for (int o = 1; o < n; o++) { //symbol 5
-                            if (o>i&&o>j)
+                        for (int o = 1; o <= n; o++) { //symbol 5
+                            if ( o > i && o > j)
                             {
                             System.out.printf("%d%d%c%c%d ", i, j, k, m, o);}
                         }
