ad92a01a1bb529d5c9b8aaa6919798a92df360ed
TV <radoslav_radev@hotmail.com>
2020-04-29 15:43:04 +0300

    Коледна надпревара

--- NestedLoops/src/TournamentForChristmass.java
+++ NestedLoops/src/TournamentForChristmass.java
@@ -4,36 +4,45 @@ public class TournamentForChristmass {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int days = Integer.parseInt (scan.nextLine ());
-        boolean win     = false;
-        boolean winT    = false;
-        double  money   = 0;
-        for (int i = 1; i <= days; i++) {
-            String event = scan.nextLine ();
-            double daymMoney = 0.0;
-            while (!event.equals ("Finish")) {
-                String result = scan.nextLine ();
-                if (result.equals ("win")) {
-                    win = true;
-                    daymMoney += 20;
-                } else if (result.equals ("lose")) {
-                    win = false;
-                } else if (result.equals ("Finish")) {
-                    break;
+        int days = Integer.parseInt(scan.nextLine());
+        double totalSum = 0;
+        int daysWin = 0;
+        int daysLose = 0;
+
+        for (int day = 1; day <= days; day++) {
+            int countWins = 0;
+            int countLoses = 0;
+            double sumPerDay = 0;
+            String game = scan.nextLine();
+            while (!"Finish".equals(game)) {
+                String result = scan.nextLine();
+
+                if ("win".equals(result)) {
+                    countWins++;
+                    sumPerDay += 20;
+                } else if ("lose".equals(result)) {
+                    countLoses++;
                 }
+
+                game = scan.nextLine();
             }
-            if (win) {
-                money += daymMoney * 1.1;
-                winT = true;
+
+            if (countWins > countLoses) {
+                sumPerDay = sumPerDay + sumPerDay * 0.1;
+                daysWin++;
+
             } else {
-                money += daymMoney;
-                winT = false;
+                daysLose++;
             }
+
+            totalSum = totalSum + sumPerDay;
         }
-        if (!winT) {
-            System.out.printf ("You lost the tournament! Total raised money: %.2f",money);
+
+        if (daysWin > daysLose) {
+            totalSum = totalSum + totalSum * 0.2;
+            System.out.printf("You won the tournament! Total raised money: %.2f", totalSum);
         } else {
-            System.out.printf ("You won the tournament! Total raised money: %.2f",money * 1.2);
+            System.out.printf("You lost the tournament! Total raised money: %.2f", totalSum);
         }
     }
 }
